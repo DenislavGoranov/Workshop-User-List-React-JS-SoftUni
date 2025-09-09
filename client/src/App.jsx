02c6d155892a0194bc14d15b5872c9fbde641e81
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-09 21:49:17 +0300

    add dynamic users information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import Main from './components/Main'
+import Main from './components/Main/Main'
 import './App.css'
 import Footer from './components/Footer'
 
