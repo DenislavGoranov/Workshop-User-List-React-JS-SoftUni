5d587084457c3d331b7df063bd50675249e4a6bb
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-11 14:44:13 +0300

    some path changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import Header from './components/Header'
 import Main from './components/Main/Main'
 import './App.css'
-import Footer from './components/Footer'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
